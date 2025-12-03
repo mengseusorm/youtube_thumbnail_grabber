acf59f8d608cffb7be6927b7bcb3e8fd84b3e7ed
Sorm Mengseu <sormmengseu96@gmail.com>
2025-12-03 16:27:34 +0700

    update

--- src/app/components/AdBanner.tsx
+++ src/app/components/AdBanner.tsx
@@ -2,6 +2,12 @@
 
 import { useEffect } from 'react';
 
+declare global {
+  interface Window {
+    adsbygoogle: any[];
+  }
+}
+
 interface AdBannerProps {
   dataAdSlot: string;
   dataAdFormat?: string;
